fa496d4b5d9c703d5d86c40a52cf2a02116a5f95
ngoquan <quannv@thanglongmedia.net>
2016-10-27 15:40:43 +0700

    Tạo navigation bar trong Android

---
+++ DemoMaterialDesign/app/src/main/java/com/example/ngoquan/demomaterialdesign/activity/FriendsFragment.java
@@ -0,0 +1,43 @@
+package com.example.ngoquan.demomaterialdesign.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ngoquan.demomaterialdesign.R;
+
+/**
+ * Created by NGOQUAN on 12/21/2015.
+ */
+public class FriendsFragment extends Fragment {
+    public FriendsFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
